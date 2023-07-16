3a20181a3f975edd77a28da4a1991879cbb96b79
Anant <anantsinghraghuvanshi1@gmail.com>
2023-07-16 19:04:25 +0530

    updating issues

--- src/main/java/com/instagrocer/tastyGreens/Security/SecurityConfiguration.java
+++ src/main/java/com/instagrocer/tastyGreens/Security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.instagrocer.tastyGreens.security;
+package com.instagrocer.tastyGreens.Security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
