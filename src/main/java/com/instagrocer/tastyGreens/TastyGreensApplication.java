864044fb7d42a3859a42414ea9a39844032aa6b1
Anant <anantsinghraghuvanshi1@gmail.com>
2023-07-16 16:46:24 +0530

    initial commit

---
+++ src/main/java/com/instagrocer/tastyGreens/TastyGreensApplication.java
@@ -0,0 +1,13 @@
+package com.instagrocer.tastyGreens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyGreensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyGreensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instagrocer/tastyGreens/TastyGreensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instagrocer.tastyGreens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastyGreensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
