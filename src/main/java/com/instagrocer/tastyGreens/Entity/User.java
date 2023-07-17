213b2a49a10dd87ebd8aa97dc5687faa0b879fba
Anant <anantsinghraghuvanshi1@gmail.com>
2023-07-17 15:34:25 +0530

    Generated value strategy add

--- src/main/java/com/instagrocer/tastyGreens/Entity/User.java
+++ src/main/java/com/instagrocer/tastyGreens/Entity/User.java
@@ -1,6 +1,8 @@
 package com.instagrocer.tastyGreens.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.Set;
@@ -9,6 +11,7 @@ import java.util.Set;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String userName;
     private String password;
