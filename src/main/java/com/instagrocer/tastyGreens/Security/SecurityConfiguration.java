6206f7bd292d69bc401c1bd8a4ec288678e22514
Anant <anantsinghraghuvanshi1@gmail.com>
2023-07-17 14:07:38 +0530

    SecurityConfiguration error resolved

--- src/main/java/com/instagrocer/tastyGreens/Security/SecurityConfiguration.java
+++ src/main/java/com/instagrocer/tastyGreens/Security/SecurityConfiguration.java
@@ -2,9 +2,12 @@ package com.instagrocer.tastyGreens.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -12,13 +15,20 @@ public class SecurityConfiguration  {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/catalog").permitAll()
-                .antMatchers("/admin/items").hasRole("ADMIN")
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll();
+        http
+                .authorizeRequests((authorizeRequests) ->
+                {
+                    try {
+                        authorizeRequests
+                .requestMatchers("/catalog").permitAll()
+                .requestMatchers("/admin/items").hasRole("ADMIN").and()
+                .formLogin(withDefaults());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+
+                return http.build();
     }
 
 }
